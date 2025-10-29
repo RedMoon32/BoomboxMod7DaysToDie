586838ea8b91a917d413e42f251324148653c69f
rb <rinat.babichev@gmail.com>
2025-10-29 14:35:08 +0300

    try fix

--- Code/BoomboxModApi.cs
+++ Code/BoomboxModApi.cs
@@ -8,51 +8,10 @@ namespace Boombox
         public void InitMod(Mod modInstance)
         {
             Debug.Log("[Boombox] Mod API initialized");
-            RegisterNetPackages();
 
             ModEvents.GameStartDone.RegisterHandler(OnGameStartDone);
             ModEvents.WorldShuttingDown.RegisterHandler(OnWorldShuttingDown);
             ModEvents.PlayerSpawnedInWorld.RegisterHandler(OnPlayerSpawnedInWorld);
-        }
-
-        private static void RegisterNetPackages()
-        {
-            RegisterPackageWithLog("ToggleRequest", BoomboxNetPackageIds.ToggleRequest, typeof(NetPackageBoomboxToggleRequest));
-            RegisterPackageWithLog("Play", BoomboxNetPackageIds.Play, typeof(NetPackageBoomboxPlay));
-            RegisterPackageWithLog("Stop", BoomboxNetPackageIds.Stop, typeof(NetPackageBoomboxStop));
-            RegisterPackageWithLog("Sync", BoomboxNetPackageIds.Sync, typeof(NetPackageBoomboxSync));
-        }
-
-        private static void RegisterPackageWithLog(string name, int id, Type packageType)
-        {
-            TryRegisterPackage(id, packageType);
-            try
-            {
-                var resolvedId = NetPackageManager.GetPackageId(packageType);
-                Debug.Log($"[Boombox] NetPackage '{name}' resolved ID {resolvedId} (expected {id})");
-            }
-            catch (Exception ex)
-            {
-                Debug.LogError($"[Boombox] Failed to resolve NetPackage '{name}' (expected ID {id}): {ex}");
-            }
-        }
-
-        private static void TryRegisterPackage(int id, Type packageType)
-        {
-            try
-            {
-                var name = NetPackageManager.GetPackageName(id);
-                if (!string.IsNullOrEmpty(name))
-                {
-                    return;
-                }
-            }
-            catch (Exception)
-            {
-                // ignored, we will register mapping next.
-            }
-
-            NetPackageManager.AddPackageMapping(id, packageType);
         }
 
         private static void OnGameStartDone(ref ModEvents.SGameStartDoneData data)
@@ -85,7 +44,7 @@ namespace Boombox
                 return;
             }
 
-            BoomboxAudioManager.ServerSyncClient(data.ClientInfo);
+            //BoomboxAudioManager.ServerSyncClient(data.ClientInfo);
         }
 
         private static bool IsServer
